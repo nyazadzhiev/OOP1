88473235d7c144433e59d838f363416e0c93ef3d
nyazadzhiev <nyazadzhiev@gmail.com>
2022-03-29 19:42:38 +0300

    Testing

--- CommandLine/src/com/company/Main.java
+++ CommandLine/src/com/company/Main.java
@@ -7,18 +7,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        FileEditor fileEditor = new FileEditor();
+        Scanner input = new Scanner(System.in);
+        CommandService commandService = new CommandService();
+        String command;
 
-        fileEditor.open("file.xml");
-        fileEditor.open("file.xml");
-        fileEditor.close();
-        fileEditor.close();
-        fileEditor.append();
-        String path = "D:" + File.separator + "Test" + File.separator + "test.txt";
-        fileEditor.saveAs(path);
-        fileEditor.close();
-        fileEditor.open(path);
-        fileEditor.append();
-        fileEditor.saveAs(path);
+        do {
+            command = input.nextLine();
+            commandService.executeCommand(command, "file.xml");
+        }while(!command.equals("exit"));
     }
 }
