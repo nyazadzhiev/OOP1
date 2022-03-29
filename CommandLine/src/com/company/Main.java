0bf828692d0c8e4f73433816048b930dbd2b8644
nyazadzhiev <nyazadzhiev@gmail.com>
2022-03-29 18:57:25 +0300

    Second testing

--- CommandLine/src/com/company/Main.java
+++ CommandLine/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,6 +14,10 @@ public class Main {
         fileEditor.open("file.xml");
         fileEditor.append();
         String path = "D:" + File.separator + "Test" + File.separator + "test.txt";
+        fileEditor.saveAs(path);
+        fileEditor.close();
+        fileEditor.open(path);
+        fileEditor.append();
         fileEditor.saveAs(path);
     }
 }
