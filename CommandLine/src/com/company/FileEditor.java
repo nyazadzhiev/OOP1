f687b994df973ed52272e628ada2b04b3e2c2317
nyazadzhiev <nyazadzhiev@gmail.com>
2022-03-29 19:05:20 +0300

    Added validation

--- CommandLine/src/com/company/FileEditor.java
+++ CommandLine/src/com/company/FileEditor.java
@@ -7,6 +7,7 @@ public class FileEditor {
     private ReadFile read;
     private EditFile edit;
     private SaveFile save;
+    private Validation validation;
     private File file;
     private StringBuilder data;
 
@@ -14,10 +15,16 @@ public class FileEditor {
         this.read = new ReadFile();
         this.edit = new EditFile();
         this.save = new SaveFile();
+        this.validation = new Validation();
         this.data = new StringBuilder();
     }
 
     public void open(String fileData) {
+        if(validation.isFileOpened(this.file)) {
+            System.out.println("You have opened file");
+
+            return;
+        }
         try {
             this.file = read.openFile(fileData, this.data);
 
@@ -28,7 +35,7 @@ public class FileEditor {
     }
 
     public void close() {
-        if(this.file != null) {
+        if(validation.isFileOpened(this.file)) {
             this.file = (File) read.closeFile();
             this.data.setLength(0);
 
@@ -39,14 +46,31 @@ public class FileEditor {
     }
 
     public void append() {
+        if(!validation.isFileOpened(this.file)) {
+            System.out.println("You don't have opened file");
+
+            return;
+        }
+
         edit.append(this.data);
     }
 
     public void save() {
+        if(!validation.isFileOpened(this.file)) {
+            System.out.println("You don't have opened file");
+
+            return;
+        }
+
         save.save(this.file, String.valueOf(this.data));
     }
 
     public void saveAs(String fileData) {
+        if(!validation.isFileOpened(this.file)) {
+            System.out.println("You don't have opened file");
+
+            return;
+        }
         save.saveAs(fileData, String.valueOf(this.data));
     }
 }
