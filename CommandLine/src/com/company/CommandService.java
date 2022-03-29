854d0ed8216e7bd9952e09cde576cd765a9d0d7b
nyazadzhiev <nyazadzhiev@gmail.com>
2022-03-29 19:41:20 +0300

    Implemented command layer

---
+++ CommandLine/src/com/company/CommandService.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class CommandService {
+    private FileEditor fileEditor;
+    private Validation validation;
+
+    public CommandService() {
+        this.validation = new Validation();
+        this.fileEditor = new FileEditor();
+    }
+
+    public void executeCommand(String command, String fileData) {
+        if(validation.isNullOrEmpty(command) || validation.isNullOrEmpty(fileData)){
+            System.out.println("you entered empty string");
+
+            return;
+        }
+
+        switch (command) {
+            case "open" : fileEditor.open(fileData); break;
+            case "close" : fileEditor.close(); break;
+            case "save" : fileEditor.save(); break;
+            case "saveas" : fileEditor.saveAs(fileData); break;
+            case "append" : fileEditor.append();
+            case "exit" : return;
+            default:
+                System.out.println("Invalid command");
+        }
+    }
+}
